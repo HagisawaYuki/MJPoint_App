bf3b6b16805e2db0bd9607cd058c90bb412a0dd0
HagisawaYuki <hagisawa7110@gmail.com>
2025-04-30 02:44:44 +0900

    commit for deploy3

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,3 +6,17 @@ const prisma = new PrismaClient()
 
 export default prisma;
 // export { prisma };
+
+// import { PrismaClient } from '../app/generated/prisma/client';
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+//   });
+
+// if (process.env.NODE_ENV === 'development') globalForPrisma.prisma = prisma;
